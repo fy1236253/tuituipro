fb3280d33becc317daf77387a9ce8a661fc917dd
fengya <zhangfengya@yunlaba.com>
2017-06-20 20:32:12 +0800

    wx

--- src/component/section/webapi.go
+++ src/component/section/webapi.go
@@ -11,7 +11,7 @@ import (
 // ReturnAuthorizerInfo 返回web的info
 func ReturnAuthorizerInfo(info string) {
 	log.Println(info)
-	addr := "http://localhost:6062/component/returninfo"
+	addr := "http://127.0.0.1:4200/authorizer_info"
 	req := httplib.Post(addr).SetTimeout(5*time.Second, 1*time.Minute)
 	req.Body(info)
 	resp, err := req.String()
