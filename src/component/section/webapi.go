372cabc35930a63965a626a803c2fab1827e63ac
fengya <zhangfengya@yunlaba.com>
2017-07-04 09:32:52 +0800

    wx

--- src/component/section/webapi.go
+++ src/component/section/webapi.go
@@ -34,6 +34,6 @@ func IsBindInfo(wxid, openid string) (respson *BindInfoRes) {
 		log.Println("[error]:", err)
 	}
 	json.Unmarshal([]byte(resp), &respson)
-	log.Println(respson)
+	log.Println(string(resp))
 	return respson
 }
