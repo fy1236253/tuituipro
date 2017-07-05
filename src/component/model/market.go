e0f15f194baa27159807ad4139dc7c6049552be4
fengya <zhangfengya@yunlaba.com>
2017-07-05 23:20:13 +0800

    wx

--- src/component/model/market.go
+++ src/component/model/market.go
@@ -3,6 +3,7 @@
 package model
 
 import (
+	"cfg"
 	"crypto/md5"
 	"encoding/hex"
 	"log"
@@ -74,7 +75,7 @@ func WeixinPay(uuid, openid, val string) {
 	o.ActName = "推推积分兑换"
 	o.Remark = "积分兑换"
 	o.NonceStr = nonce
-	// log.Println(cfg.Config().WeiXinPay.Ip)
+	log.Println(cfg.Config())
 	log.Println(o)
 	// o.Sign = sign(o, cfg.Config().WeiXinPay.Key)
 	// buf := bytes.NewBuffer(make([]byte, 0, 16<<10))
